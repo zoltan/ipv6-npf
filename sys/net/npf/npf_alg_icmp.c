190844447e9f879bfb6d4b158d3146bd4b2c2400
Zoltan Arnold NAGY <zoltan@netbsd.org>
2011-07-03 02:00:53 +0200

    fix typo

--- sys/net/npf/npf_alg_icmp.c
+++ sys/net/npf/npf_alg_icmp.c
@@ -120,7 +120,7 @@ npfa_icmp_match(npf_cache_t *npc, nbuf_t *nbuf, void *ntptr)
 	in_port_t dport;
 
 	KASSERT(npf_iscached(npc, NPC_IP46));
-	KASSERT(npf_iscahced(npc, NPC_LAYER4));
+	KASSERT(npf_iscached(npc, NPC_LAYER4));
 
 	/* Check for low TTL. */
 	if (ip->ip_ttl > TR_MAX_TTL) {
